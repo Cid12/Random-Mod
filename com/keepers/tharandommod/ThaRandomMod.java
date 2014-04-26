1cd0c479002ff86b882cdba0663f50c1fc749dd6
KeeperofMee <kim.thysell@live.se>
2014-04-27 01:32:46 +0200

    Added 7 trapdoors

--- com/keepers/tharandommod/ThaRandomMod.java
+++ com/keepers/tharandommod/ThaRandomMod.java
@@ -31,26 +31,56 @@ public class ThaRandomMod {
         @SidedProxy(clientSide="com.keepers.tharandommod.proxy.ClientProxy", serverSide="com.keepers.tharandommod.proxy.CommonProxy")
         public static CommonProxy proxy;
         
+        //Blocks
+        
+        //Wooden hidden trap doors
+        public static Block hiddenTrapDoor;
+        public static Block hiddenTrapDoor1;
+        public static Block hiddenTrapDoor2;
+        public static Block hiddenTrapDoor3;
+        public static Block hiddenTrapDoor4;
+        public static Block hiddenTrapDoor5;
+        
+        //Stone hidden trap doors
+        public static Block hiddenTrapDoor6;
+        public static Block hiddenTrapDoor7;
+        
         //Adding creative tab randomTab to minecraft
-        public static CreativeTabs randomTab = new CreativeTabs("randomTab") {
+        public static CreativeTabs randomtTrapTab = new CreativeTabs("randomTrapTab") {
 			
 			public Item getTabIconItem() {
-				return Items.apple;
+				return Item.getItemFromBlock(hiddenTrapDoor);
 			}
     };
     
-    //Blocks
-    public static Block hiddenTrapDoor;
+
+
        
         @EventHandler
         public void preInit(FMLPreInitializationEvent event) {
-                //Put code here;
         	
         	//Defines blocks in Minecraft
-        	hiddenTrapDoor = new TrmbHiddenTrapDoor(Material.wood);
+        	//Wooden hidden trap doors
+        	hiddenTrapDoor = new TrmbHiddenTrapDoor(Material.wood, 0).setBlockName("hiddenTrapDoor");
+        	hiddenTrapDoor1 = new TrmbHiddenTrapDoor(Material.wood, 1).setBlockName("hiddenTrapDoorBirch");
+        	hiddenTrapDoor2 = new TrmbHiddenTrapDoor(Material.wood, 2).setBlockName("hiddenTrapDoorSpruce");
+        	hiddenTrapDoor3 = new TrmbHiddenTrapDoor(Material.wood, 3).setBlockName("hiddenTrapDoorJungle");
+        	hiddenTrapDoor4 = new TrmbHiddenTrapDoor(Material.wood, 4).setBlockName("hiddenTrapDoorAcacia");
+        	hiddenTrapDoor5 = new TrmbHiddenTrapDoor(Material.wood, 5).setBlockName("hiddenTrapDoorBigOak");
+        	//Stone hidden trap doors
+        	hiddenTrapDoor6 = new TrmbHiddenTrapDoor(Material.rock, 6).setBlockName("hiddenTrapDoorStone");
+        	hiddenTrapDoor7 = new TrmbHiddenTrapDoor(Material.rock, 7).setBlockName("hiddenTrapDoorCobblestone");
         	
         	//Registers blocks in Minecraft
         	GameRegistry.registerBlock(hiddenTrapDoor, "hiddenTrapDoor");
+        	GameRegistry.registerBlock(hiddenTrapDoor1, "hiddenTrapDoor1");
+        	GameRegistry.registerBlock(hiddenTrapDoor2, "hiddenTrapDoor2");
+        	GameRegistry.registerBlock(hiddenTrapDoor3, "hiddenTrapDoor3");
+        	GameRegistry.registerBlock(hiddenTrapDoor4, "hiddenTrapDoor4");
+        	GameRegistry.registerBlock(hiddenTrapDoor5, "hiddenTrapDoor5");
+        	GameRegistry.registerBlock(hiddenTrapDoor6, "hiddenTrapDoor6");
+        	GameRegistry.registerBlock(hiddenTrapDoor7, "hiddenTrapDoor7");
+
         }
         
         @EventHandler
--- com/keepers/tharandommod/blocks/TrmbHiddenTrapDoor.java
+++ com/keepers/tharandommod/blocks/TrmbHiddenTrapDoor.java
@@ -3,18 +3,50 @@ package com.keepers.tharandommod.blocks;
 import com.keepers.tharandommod.ThaRandomMod;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockTrapDoor;
 import net.minecraft.block.material.Material;
 
-public class TrmbHiddenTrapDoor extends Block {
+public class TrmbHiddenTrapDoor extends BlockTrapDoor {
 
 	ThaRandomMod m = new ThaRandomMod() ;
-	public TrmbHiddenTrapDoor(Material mat) {
+	public TrmbHiddenTrapDoor(Material mat, int type) {
 		super(mat);
-		setStepSound(soundTypeWood);
-		setBlockTextureName("planks_oak");
-		setCreativeTab(m.randomTab);
-		setBlockName("hiddenTrapDoor");
 		setHardness(1.0F);
+		setCreativeTab(m.randomtTrapTab);
+		
+		if(type == 0)
+			setBlockTextureName("planks_oak");
+		setStepSound(soundTypeWood);
+		
+		if(type == 1)
+			 setBlockTextureName("planks_birch");
+		setStepSound(soundTypeWood);
+		
+		if(type == 2)
+			 setBlockTextureName("planks_spruce");
+		setStepSound(soundTypeWood);
+		
+		if(type == 3)
+			 setBlockTextureName("planks_jungle");
+		setStepSound(soundTypeWood);
+		
+		if(type == 4)
+			 setBlockTextureName("planks_acacia");
+		setStepSound(soundTypeWood);
+		
+		if(type == 5)
+			 setBlockTextureName("planks_big_oak");
+		setStepSound(soundTypeWood);
+		
+		if(type == 6)
+			 setBlockTextureName("stone");
+		setStepSound(soundTypePiston);
+		
+		if(type == 7)
+			 setBlockTextureName("cobblestone");
+		setStepSound(soundTypePiston);
+		
+		
 	}
 
 }
